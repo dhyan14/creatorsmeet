17b02fabaa880457cdd60ea80b7303a1cff0a5b0
dhyan14 <dhyanjain341988@gmail.com>
2025-03-23 15:31:23 +0530

    JH

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from "@prisma/client/runtime/library";
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
